0cb8d5e831fcc329e8ec974f5d1efeedd1febda2
Lifeskills-hub <lifeskills.orient@gmail.com>
2026-01-13 09:50:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ locations.forEach((loc, index) => {
 
 let currentStage = 0;
 let unlockedStage = 1;
-const TOTAL_STAGES_NEEDED = 6;          // ← Win after completing any 6 stations
+const TOTAL_STAGES_NEEDED = 10;          // ← Win after completing any 6 stations
 let team = '';
 let className = '';
 const markers = [];
